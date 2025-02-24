58a3f5f039159ce91822566935875acb88be0845
Kai S <kais@Kais-MacBook-Pro.local>
2025-02-24 11:05:42 +0100

    lock extension

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook'
 /**
  * Initialization data for the cell-lock extension.
  */
@@ -10,8 +12,44 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'cell-lock:plugin',
   description: 'A JupyterLab extension for cell lock.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  activate: (app: JupyterFrontEnd, notebookTracker: INotebookTracker) => {
     console.log('JupyterLab extension cell-lock is activated!');
+
+    // Disable the "insert above" command
+    app.commands.addCommand('notebook:insert-cell-above', {
+      execute: () => {
+        console.warn('Adding cells above is disabled.');
+      },
+      label: 'Insert Cell Above',
+      isEnabled: () => false,
+    });
+
+    // Disable the "insert below" command
+    app.commands.addCommand('notebook:insert-cell-below', {
+      execute: () => {
+        console.warn('Adding cells below is disabled.');
+      },
+      label: 'Insert Cell Below',
+      isEnabled: () => false, 
+    });
+
+    // Disable the "insert cell" command
+    app.commands.addCommand('notebook:insert-cell', {
+      execute: () => {
+        console.warn('Adding new cells is disabled.');
+      },
+      label: 'Insert Cell',
+      isEnabled: () => false, 
+    });
+
+    // Disable the "duplicate cell below" command
+    app.commands.addCommand('notebook:duplicate-below', {
+      execute: () => {
+        console.warn('Duplicating cells below is disabled.');
+      },
+      label: 'Duplicate Cell Below',
+      isEnabled: () => false, 
+    });
   }
 };
 
